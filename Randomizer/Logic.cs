f2c3819f2d05284b4b1bd407f3c0666bbd88f3f8
Mikael <m.klaening@gmail.com>
2020-03-04 10:37:19 +0100

    App asks for names and divides into two groups

---
+++ Randomizer/Logic.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomizer
+{
+    class Logic
+    {
+        internal static int EnterQtyElements()
+        {
+            bool entryValid;
+            int qtyPeople = 0;
+            do
+            {
+                entryValid = true;
+                Console.Write("Enter how any people you want to randomize: ");
+                try
+                {
+                    qtyPeople = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Something went wrong, try again!");
+                    entryValid = false;
+                }
+
+            } while (!entryValid);
+
+            return qtyPeople;
+        }
+
+        internal static List<string> EnterNames(int qtyPeople)
+        {
+            List<string> names = new List<string>();
+
+            for (int i = 0; i < qtyPeople; i++)
+            {
+                Console.Write($"Enter name of element {i + 1}: ");
+                string name = Console.ReadLine();
+                names.Add(name);
+            }
+
+            return names;
+        }
+    }
+}
---
+++ Randomizer/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomizer
+{
+    class Program
+    {
+        static Random rng = new Random();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to this randomizer app!\n");
+
+            int qtyPeople = Logic.EnterQtyElements();
+
+            List<string> names = Logic.EnterNames(qtyPeople);
+
+            int amountPeople = names.Count;
+            int minRnd = 1;
+
+            int result;
+
+            Console.WriteLine("Blåa gruppen:\n");
+
+            bool thereAreElements = names.Any();
+
+            while (thereAreElements)
+            {
+                int maxRnd = names.Count + 1;
+                result = rng.Next(minRnd, maxRnd) - 1;
+
+                Console.WriteLine(names[result]);
+                names.Remove(names[result]);
+
+                thereAreElements = names.Any();
+
+                System.Threading.Thread.Sleep(1500);
+
+                if (thereAreElements)
+                {
+                    Shuffle(names);
+                }
+                if (names.Count == amountPeople / 2)
+                {
+                    Console.WriteLine("------------");
+                    Console.WriteLine("Vita gruppen:\n");
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void Shuffle<T>(List<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
